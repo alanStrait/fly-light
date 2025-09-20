4c617d99b9a37247ce381d3818947902205320f2
alanStrait <alans@brsg.io>
2025-09-20 14:25:37 -0600

    Add machine fields memory, core, status

--- fly_kv/lib/fly_kv/machine.ex
+++ fly_kv/lib/fly_kv/machine.ex
@@ -13,7 +13,12 @@ defmodule FlyKv.Machine do
     :tls_config_ca_file,
     :tls_config_cert_file,
     :tls_config_key_file,
-    :tls_config_insecure_skip_verify
+    :tls_config_insecure_skip_verify,
+    :memory_total,
+    :memory_allocated,
+    :cores_total,
+    :cores_allocated,
+    :status
   ]
 
   @type t :: %__MODULE__{
@@ -29,7 +34,12 @@ defmodule FlyKv.Machine do
     tls_config_ca_path: binary() | nil,
     tls_config_cert_file: binary() | nil,
     tls_config_key_file: binary() | nil,
-    tls_config_insecure_skip_verify: boolean()
+    tls_config_insecure_skip_verify: boolean(),
+    memory_total: integer(),
+    memory_allocated: integer(),
+    cores_total: integer(),
+    cores_allocated: integer(),
+    status: binary()
   }
 
   NimbleCSV.define(MachineParser, separator: ",")
@@ -42,14 +52,17 @@ defmodule FlyKv.Machine do
       |> MachineParser.parse_stream()
       |> Stream.map(fn [region_code, address, scheme, datacenter, http_path_username, http_auth_password,
                         wait_time, token, tls_config_address, tls_config_ca_path, tls_config_ca_file,
-                        tls_config_cert_file, tls_config_key_file, tls_config_insecure_skip_verify] ->
+                        tls_config_cert_file, tls_config_key_file, tls_config_insecure_skip_verify,
+                        memory_total, memory_allocated, cores_total, cores_allocated, status] ->
         %{
           region_code: region_code, address: address, scheme: scheme, datacenter: datacenter,
           http_path_username: http_path_username, http_auth_password: http_auth_password,
           wait_time: String.to_integer(wait_time), token: token, tls_config_address: tls_config_address,
           tls_config_ca_path: tls_config_ca_path, tls_config_ca_file: tls_config_ca_file,
           tls_config_cert_file: tls_config_cert_file, tls_config_key_file: tls_config_key_file,
-          tls_config_insecure_skip_verify: tls_config_insecure_skip_verify
+          tls_config_insecure_skip_verify: tls_config_insecure_skip_verify,
+          memory_total: memory_total, memory_allocated: memory_allocated,
+          cores_total: cores_total, cores_allocated: cores_allocated, status: status
         }
       end)
       |> Enum.map(&struct(__MODULE__, &1))
